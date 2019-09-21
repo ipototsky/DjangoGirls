5f9b06f12cd5fa997c4a01bbd57114cb3d0b7aa7
Ivan Patotski <ivan.patotski@clarivate.com>
2019-09-21 15:18:09 +0300

    My Django Girls app, first commit

---
+++ test.py
@@ -0,0 +1,4 @@
+participant = {'name': 'Ola', 'country': 'Poland', 'favorite_numbers': [7, 42, 92]}
+# participant['favorite_language'] = 'Python'
+print(participant['name'])
+print(len(participant))
